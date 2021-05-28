e1750e466c4580693318b4227d048d705cfc54e2
Wongsapat <wongsapat.pr@gmail.com>
2021-05-28 12:14:01 +0700

    Test

---
+++ src/test/java/com/example/linebottest/LineBotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linebottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineBotTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
