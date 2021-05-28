a3ad00ecce96b16d7a970cec8e62e43cff429104
Wongsapat <wongsapat.pr@gmail.com>
2021-05-28 12:45:37 +0700

    Second commit

--- src/main/java/com/example/linebottest/controller/LinebotController.java
+++ src/main/java/com/example/linebottest/controller/LinebotController.java
@@ -1,19 +1,95 @@
 package com.example.linebottest.controller;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @LineMessageHandler
 public class LinebotController {
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
     
     @EventMapping
-    public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
-        System.out.println("event: " + e);
-        TextMessageContent message = e.getMessage();
-        return new TextMessage(message.getText());
+    public void handleTextMessage(MessageEvent<TextMessageContent> event) {
+        log.info(event.toString());
+        TextMessageContent message = event.getMessage();
+        handleTextContent(event.getReplyToken(), event, message);
+    }
+
+    private void handleTextContent(String replyToken, Event event, 
+                                   TextMessageContent content) {
+        String text = content.getText();
+
+        log.info("Got text message from %s : %s", replyToken, text);
+
+        switch (text) {
+            case "Profile": {
+                String userId = event.getSource().getUserId();
+                if(userId != null) {
+                    lineMessagingClient.getProfile(userId)
+                            .whenComplete((profile, throwable) -> {
+                                if(throwable != null) {
+                                    this.replyText(replyToken, throwable.getMessage());
+                                    return;
+                                }
+                                this.reply(replyToken, Arrays.asList(
+                                        new TextMessage("Display name: " + 
+                                                        profile.getDisplayName()),
+                                        new TextMessage("Status message: " + 
+                                                        profile.getStatusMessage()),
+                                        new TextMessage("User ID: " + 
+                                                        profile.getUserId())
+                                ));
+                            });
+                }
+                break;
+            }
+            default:
+                log.info("Return echo message %s : %s", replyToken, text);
+                this.replyText(replyToken, text);
+        }
+    }
+
+    private void replyText(@NonNull  String replyToken, @NonNull String message) {
+        if(replyToken.isEmpty()) {
+            throw new IllegalArgumentException("replyToken is not empty");
+        }
+
+        if(message.length() > 1000) {
+            message = message.substring(0, 1000 - 2) + "...";
+        }
+        this.reply(replyToken, new TextMessage(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull TextMessage textMessage) {
+        reply(replyToken, Collections.singletonList(textMessage));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+        try {
+            BotApiResponse response = lineMessagingClient.replyMessage(
+                    new ReplyMessage(replyToken, messages)
+            ).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
