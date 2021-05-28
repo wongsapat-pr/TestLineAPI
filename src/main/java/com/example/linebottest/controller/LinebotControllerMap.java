ec7d7202d7461211325fb6acc3b2860eb5feb9b9
Wongsapat <wongsapat.pr@gmail.com>
2021-05-28 13:27:09 +0700

    sixth commit

---
+++ src/main/java/com/example/linebottest/controller/LinebotControllerMap.java
@@ -0,0 +1,45 @@
+package com.example.linebottest.controller;
+
+import java.util.concurrent.ExecutionException;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.LocationMessageContent;
+import com.linecorp.bot.model.message.LocationMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@LineMessageHandler
+public class LinebotControllerMap {
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+    
+    @EventMapping
+    public void handleLocationMessage(MessageEvent<LocationMessageContent> event) {
+    log.info(event.toString());
+    LocationMessageContent message = event.getMessage();
+    reply(event.getReplyToken(), new LocationMessage(
+            (message.getTitle() == null) ? "Location replied" : message.getTitle(),
+            message.getAddress(),
+            message.getLatitude(),
+            message.getLongitude()
+    ));
+}
+
+    private void reply(String replyToken, LocationMessage locationMessage) {
+        try {
+            BotApiResponse response = lineMessagingClient.replyMessage(
+                    new ReplyMessage(replyToken, locationMessage)
+            ).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
