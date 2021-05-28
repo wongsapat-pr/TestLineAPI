1038825528542d05cff8c4a776104e7db9e5cac3
Wongsapat <wongsapat.pr@gmail.com>
2021-05-28 12:35:23 +0700

    Edit file

---
+++ src/main/java/com/example/linebottest/controller/LinebotController.java
@@ -0,0 +1,19 @@
+package com.example.linebottest.controller;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@LineMessageHandler
+public class LinebotController {
+    
+    @EventMapping
+    public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
+        System.out.println("event: " + e);
+        TextMessageContent message = e.getMessage();
+        return new TextMessage(message.getText());
+    }
+}
