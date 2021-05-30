44ca31a440f8166413ecd24100fe2421dbbd46c5
Wongsapat <wongsapat.pr@gmail.com>
2021-05-30 18:17:48 +0700

    edit Webmvc

--- src/main/java/com/example/linebottest/controller/LinebotMvcConfigurer.java
+++ src/main/java/com/example/linebottest/controller/LinebotMvcConfigurer.java
@@ -1,14 +1,16 @@
-package com.example.linebottest.config;
+package com.example.linebottest.controller;
+
 import com.example.linebottest.LineBotTestApplication;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Configuration
-public class LinebotImage implements WebMvcConfigurer{
+@Slf4j
+public class LinebotMvcConfigurer implements WebMvcConfigurer{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String downloadedContentUri = LineBotTestApplication.downloadedContentDir.toUri().toASCIIString();
